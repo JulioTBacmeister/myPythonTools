e2fbde123b90475c74daaf8e597e58f9e8f0e932
JulioTBacmeister <juliob@ucar.edu>
2022-10-25 12:39:03 -0600

    updating supersimple

--- MachineLearning/tfsupersimple.py
+++ MachineLearning/tfsupersimple.py
@@ -2,9 +2,10 @@ import tensorflow as tf
 
 import matplotlib.pyplot as plt
 
-colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
 
-
+############################################################
+#  Function Defs
+############################################################
 class MyModel(tf.Module):
   def __init__(self, **kwargs):
     super().__init__(**kwargs)
@@ -24,6 +25,46 @@ def loss(target_y, predicted_y):
 def f(x):
   return x * TRUE_W + TRUE_B
 
+
+# Given a callable model, inputs, outputs, and a learning rate...
+def train(model, x, y, learning_rate):
+
+  with tf.GradientTape() as t:
+    # Trainable variables are automatically tracked by GradientTape
+    current_loss = loss(y, model(x))
+
+  # Use GradientTape to calculate the gradients with respect to W and b
+  dw, db = t.gradient(current_loss, [model.w, model.b])
+
+  # Subtract the gradient scaled by the learning rate
+  model.w.assign_sub(learning_rate * dw)
+  model.b.assign_sub(learning_rate * db)
+
+# Define a training loop
+def report(model, loss):
+  return f"W = {model.w.numpy():1.2f}, b = {model.b.numpy():1.2f}, loss={loss:2.5f}"
+
+
+def training_loop(model, x, y):
+
+  for epoch in epochs:
+    # Update the model with the single giant batch
+    train(model, x, y, learning_rate=0.1)
+
+    # Track this before I update
+    weights.append(model.w.numpy())
+    biases.append(model.b.numpy())
+    current_loss = loss(y, model(x))
+
+    print(f"Epoch {epoch:2d}:")
+    print("    ", report(model, current_loss))
+
+
+#####################################################################
+#     Run stuff
+#####################################################################
+
+colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
 
 model = MyModel()
 
@@ -50,12 +91,6 @@ noise = tf.random.normal(shape=[NUM_EXAMPLES])
 # Calculate y
 y = f(x) + noise
 
-# Plot all the data
-#plt.plot(x, y, '.')
-#plt.show()
-
-
-
 
 plt.plot(x, y, '.', label="Data")
 plt.plot(x, f(x), label="Ground truth")
@@ -64,3 +99,33 @@ plt.legend()
 plt.show()
 
 print("Current loss: %1.6f" % loss(y, model(x)).numpy())
+
+
+# Collect the history of W-values and b-values to plot later
+weights = []
+biases = []
+epochs = range(10)
+
+
+##
+
+current_loss = loss(y, model(x))
+
+print(f"Starting:")
+print("    ", report(model, current_loss))
+
+
+training_loop(model, x, y)
+
+
+
+plt.plot(epochs, weights, label='Weights', color=colors[0])
+plt.plot(epochs, [TRUE_W] * len(epochs), '--',
+         label = "True weight", color=colors[0])
+
+plt.plot(epochs, biases, label='bias', color=colors[1])
+plt.plot(epochs, [TRUE_B] * len(epochs), "--",
+         label="True bias", color=colors[1])
+
+plt.legend()
+plt.show()
