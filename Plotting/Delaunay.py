f1eddda46c50bceeb356c46f077bb03f62a5d2d3
JulioTBacmeister <juliob@ucar.edu>
2022-11-19 16:36:00 -0700

    foo

--- Plotting/Delaunay.py
+++ Plotting/Delaunay.py
@@ -3,14 +3,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.spatial import Delaunay
 
-def examplot():
+def examplot(npoints=100):
 
     rng = np.random.default_rng()
-    x = rng.random(10) - 0.5
-    y = rng.random(10) - 0.5
+    x = rng.random(npoints) - 0.5
+    y = rng.random(npoints) - 0.5
     z = np.hypot(x, y)
-    X = np.linspace(min(x), max(x)) # linspace defaults to 50 samples
-    Y = np.linspace(min(y), max(y))
+    z = rng.random(npoints) 
+    X = np.linspace(min(x), max(x), num=100) # linspace defaults to 50 samples
+    Y = np.linspace(min(y), max(y), num=100)
 
     XX, YY = np.meshgrid(X, Y)
     
@@ -18,31 +19,44 @@ def examplot():
     poo=Delaunay( ooo )
     interp = LinearNDInterpolator( poo , z)
     Z1=interp(XX,YY)
-    
+
+    clevels=np.linspace(0.,1.,num=20)
     
     interp2 = LinearNDInterpolator( list(zip(x,y)), z)
     Z2=interp2(XX,YY)
     
-    
+    ZOG = np.hypot(XX, YY)
+
     plt.ion()
 
-    plt.pcolormesh(XX, YY, Z2 )
+    plt.figure(1).clear
+    plt.pcolormesh(XX, YY, ZOG ,vmin=0, vmax=1.)
     plt.colorbar()
-    plt.plot(x, y, "ok", label="input point")
-    plt.legend()
-    plt.title(" zip(x,y) input " )
+    plt.contour(XX, YY, ZOG, levels=clevels, colors='black' )
+    #plt.plot(x, y, "ok", label="input point")
+    #plt.legend()
+    #plt.title(" zip(x,y) input " )
+    plt.title(" Gridded input " )
     plt.axis("equal") # gives aspect ratio=1
 
     plt.figure(2)
 
-    plt.pcolormesh(XX, YY, Z1 )
+    plt.pcolormesh(XX, YY, Z1 ,vmin=0, vmax=1. )
     plt.colorbar()
+    #plt.contour(XX, YY, Z1, levels=clevels, colors='gray' )
     plt.plot(x, y, "ok", label="input point")
-    plt.legend()
+    #plt.legend()
     plt.title(" Precomputed Delaunay input " )
     plt.axis("equal")
 
     diff=Z1-Z2
     #print(diff[:,24])
 
+    plt.figure(3)
+
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+
+    ax.plot_surface(XX, YY, Z1 ,vmin=0, vmax=1. )
+
     
+    return(Z1,XX,YY)
