4217ce8d1dad9bab77b88cf97747f20c8b070f7d
Julio Bacmeister <juliob@ucar.edu>
2023-01-13 23:35:16 -0700

    some topo stuff, tow-exp nb ...

--- GenlTools/AveragingUtils.py
+++ GenlTools/AveragingUtils.py
@@ -75,3 +75,52 @@ def SeasonalZonal( ds, season, **kwargs ):
         print(" 3D lev-lat-lon variable " )
     
     return A_mmm_zon
+
+def Seasonal( ds, season, **kwargs ):
+    
+    if 'fld' in kwargs:
+        fld = kwargs['fld']
+        A = ds[fld]
+    elif 'data' in kwargs:
+        A = kwargs['data']
+    else:
+        assert A is not None, "A needs to gien "
+    
+    if 'dims' in kwargs:
+        VarDims = kwargs['dims']
+    else:
+        VarDims = 'levlatlon'
+    
+    
+    
+    A=ds[fld]
+    time_bnds = ds['time_bnds']
+    date=ds['date']
+    time=ds['time']
+    lats=ds['lat']
+
+    print(np.shape(time))
+    print(time_bnds[0].values[0])
+    time0=[]
+    months=[]
+    years=[]
+    
+    for ixtime in time_bnds:
+        time0.append( ixtime.values[0] )
+        years.append( ixtime.values[0].year )
+        months.append( ixtime.values[0].month )
+    
+    imos = MonthsSeason( season=season )
+    imonths=np.asarray(months)
+    season = np.where( ( imonths== imos[0] ) | ( imonths==imos[1] ) | ( imonths==imos[2] ) )
+    print( "Indices of months", season[0] )
+    
+    if (VarDims=='levlatlon'):
+        A_mmm = np.average( A[ season[0] ,:,:,:], axis=0 )
+        print(" 3D lev-lat-lon variable " )
+    if (VarDims=='latlon'):
+        A_mmm = np.average( A[ season[0] ,:,:], axis=0 )
+        print(" 2D lev-lat-lon variable " )
+
+        
+    return A_mmm
